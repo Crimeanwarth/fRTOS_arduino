13f806cf98c3ec898d08ade228f154bce161cbb7
deniztohumcu <deniz.tohumcu93@gmail.com>
2020-12-09 04:00:06 +0100

    Sketch Converted to FreeRTOS

--- sketch_dec09a/sketch_dec09a.ino
+++ sketch_dec09a/sketch_dec09a.ino
@@ -1,3 +1,5 @@
+#include <Arduino_FreeRTOS.h>
+
 // Digital Pin configuration 
 
 #define RED    6
@@ -5,20 +7,44 @@
 #define BLUE   8
 
 void setup() {
-  // Pin mode definition 
 
-  pinMode(RED, OUTPUT);
-  pinMode(YELLOW, OUTPUT);
-  pinMode(BLUE, OUTPUT);
-  
+  xTaskCreate(redLedControllerTask,"RED LED TASK",128,NULL,1,NULL);
+  xTaskCreate(yellowLedControllerTask,"YELLOW LED TASK",128,NULL,1,NULL);
+  xTaskCreate(blueLedControllerTask,"BLUE LED TASK",128,NULL,1,NULL);
+
 }
+
+void redLedControllerTask(void *pvParameters) {
+  pinMode(RED, OUTPUT);
+    
+  while(1){
+    digitalWrite(RED, digitalRead(RED)^1);
+  }
+}
+  
+void yellowLedControllerTask(void *pvParameters) {
+  pinMode(YELLOW, OUTPUT);
+      
+  while(1){
+    digitalWrite(YELLOW, digitalRead(YELLOW)^1);
+  }
+}
+  
+void blueLedControllerTask(void *pvParameters) {
+  pinMode(BLUE, OUTPUT);
+   
+  while(1){
+    digitalWrite(BLUE, digitalRead(BLUE)^1);
+  }
+}
+  
 
 void loop() {
   // Digital Write of the pins
   
-  digitalWrite(RED, digitalRead(RED)^1);
-  digitalWrite(YELLOW, digitalRead(YELLOW)^1);
-  digitalWrite(BLUE, digitalRead(BLUE)^1);
-  delay(50);
+//  digitalWrite(RED, digitalRead(RED)^1);
+//  digitalWrite(YELLOW, digitalRead(YELLOW)^1);
+//  digitalWrite(BLUE, digitalRead(BLUE)^1);
+//  delay(50);
   
 }
