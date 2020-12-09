6e38d8d288dbec4026331ac6854f310fd1d06f26
deniztohumcu <deniz.tohumcu93@gmail.com>
2020-12-09 04:13:20 +0100

    Debugging included

--- sketch_dec09a/sketch_dec09a.ino
+++ sketch_dec09a/sketch_dec09a.ino
@@ -6,8 +6,16 @@
 #define YELLOW 7
 #define BLUE   8
 
+typedef int TaskProfiler;
+
+TaskProfiler RedLEDProfiler;
+TaskProfiler YellowLEDProfiler;
+TaskProfiler BlueLEDProfiler;
+
 void setup() {
 
+  Serial.begin(9600);
+  
   xTaskCreate(redLedControllerTask,"RED LED TASK",128,NULL,1,NULL);
   xTaskCreate(yellowLedControllerTask,"YELLOW LED TASK",128,NULL,1,NULL);
   xTaskCreate(blueLedControllerTask,"BLUE LED TASK",128,NULL,1,NULL);
@@ -18,7 +26,12 @@ void redLedControllerTask(void *pvParameters) {
   pinMode(RED, OUTPUT);
     
   while(1){
+    RedLEDProfiler++;
+    Serial.print("RedLEDProfiler : ");
+    Serial.println(RedLEDProfiler);
+    
     digitalWrite(RED, digitalRead(RED)^1);
+    delay(300);
   }
 }
   
@@ -26,7 +39,12 @@ void yellowLedControllerTask(void *pvParameters) {
   pinMode(YELLOW, OUTPUT);
       
   while(1){
+    YellowLEDProfiler++;
+    Serial.print("YellowLEDProfiler : ");
+    Serial.println(YellowLEDProfiler);
+    
     digitalWrite(YELLOW, digitalRead(YELLOW)^1);
+    delay(300);
   }
 }
   
@@ -34,7 +52,12 @@ void blueLedControllerTask(void *pvParameters) {
   pinMode(BLUE, OUTPUT);
    
   while(1){
+    BlueLEDProfiler++;
+    Serial.print("BlueLEDProfiler : ");
+    Serial.println(BlueLEDProfiler);
+    
     digitalWrite(BLUE, digitalRead(BLUE)^1);
+    delay(300);
   }
 }
   
